b8ab0f8a728a06db56f2c416948ab4d1a0090a78
Akshay Mahajan <akshaymahajan730@gmail.com>
2026-06-05 18:06:52 +0530

    changed logo

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -15,20 +15,14 @@ export const metadata: Metadata = {
   generator: 'v0.app',
   icons: {
     icon: [
+      
       {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
+        url: '/logo.jpg',
+        type: 'image/jpg',
+        sizes: '16x16',
       },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+      
+    ]
   },
 }
 
